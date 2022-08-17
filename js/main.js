dd1e19cc480f71c5147bb0391f1db71a17276804
snow2405 <hey@lionsix.ch>
2022-08-17 17:55:38 +0200

    fixed spelling

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
 //DOM ELEMENTS
 
 const time = document.getElementById('time'),
-greeting = document.getElementById('greeting'),
-name = document.getElementById('name'),
-title = document.getElementById('title'),
-focus = document.getElementById('focus');
+  greeting = document.getElementById('greeting'),
+  name = document.getElementById('name'),
+  title = document.getElementById('title'),
+  focus = document.getElementById('focus');
 
 //Show Time
 
-function showTime(){
-    let today = new Date(),
-      hour = today.getHours(),
-      min = today.getMinutes(),
-      sec = today. getSeconds();
+function showTime() {
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
 
-    //const amPm = hour >= 12 ? 'PM' : 'AM';
-    //hour = hour % 12 || 12;
+  //const amPm = hour >= 12 ? 'PM' : 'AM';
+  //hour = hour % 12 || 12;
 
-    //output 
-    time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+  //output 
+  time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
 
-    setTimeout(showTime, 1000);
+  setTimeout(showTime, 1000);
 
 }
 
-function addZero(n){
-    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
 
-function setup(){
+function setup() {
   let today = new Date(),
     hour = today.getHours();
 
-  if(hour < 12){
-   document.body.style.backgroundImage = "url('../img/morning.jpg')";
-    title.textContent = 'Good Morning';
-    greeting.textContent = 'Good Morning';
-   
-  }else if (hour < 18){
+  if (hour < 12) {
     document.body.style.backgroundImage = "url('../img/morning.jpg')";
     title.textContent = 'Good Morning';
     greeting.textContent = 'Good Morning';
-  }else{
+
+  } else if (hour < 18) {
+    document.body.style.backgroundImage = "url('../img/morning.jpg')";
+    title.textContent = 'Good Morning';
+    greeting.textContent = 'Good Morning';
+  } else {
     document.body.style.backgroundImage = "url('../img/morning.jpg')";
     title.textContent = 'Good Morning';
     greeting.textContent = 'Good Morning';
