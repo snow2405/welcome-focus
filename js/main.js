3d82fbad2b61b05b85a2afc5f85b5e8bb964c679
Lion Six <hey@lionsix.ch>
2022-08-17 14:20:00 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//DOM ELEMENTS
+
+const time = document.getElementById('time'),
+greeting = document.getElementById('greeting'),
+name = document.getElementById('name'),
+focuse = document.getElementById('focus');
+
+//Show Time
+
+function showTime(){
+    let today = new Date(),
+      hour = today.getHours(),
+      min = today.getMinutes(),
+      sec = today. getSeconds();
+
+    //const amPm = hour >= 12 ? 'PM' : 'AM';
+    //hour = hour % 12 || 12;
+
+    //output 
+    time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+
+    setTimeout(showTime, 1000);
+}
+
+function addZero(n){
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+showTime();
